451aa5193de89b974e7a38555d98859bccea9f85
Alex Koch <alex549us3@gmail.com>
2017-09-05 10:57:40 -0600

    2 and 5 done

--- src/index.js
+++ src/index.js
@@ -8,6 +8,41 @@ document.addEventListener("DOMContentLoaded", () => {
     let number = Object.values(data.word)[0]
     $('h3').replaceWith(`<h3>Top word from Word Watch API: ${word} (${number})</h3>`)
   })
+
+  $('button').on('click', function() {
+    const input = $('textarea').val()
+    const words = input.split(" ")
+    countWords(words)
+  })
+
+  function countWords(words) {
+    const count = {}
+    for (var i = 0; i < words.length; i++) {
+      var num = words[i].toLowerCase()
+      count[num] = count[num] ? count[num] + 1 : 1
+    }
+    reformatForDom(count)
+  }
+
+  function reformatForDom(count) {
+    const countKeys = Object.keys(count)
+    const countedWords = []
+    var i
+    for (i in countKeys){
+       countedWords.push([countKeys[i], count[countKeys[i]]]);
+    }
+    countedWords.forEach(function(word) {
+      appendToDom(word)
+    })
+  }
+
+  function appendToDom(word) {
+    $('.word-count').append(`<span style=font-size:${word[1]}em>${word[0]}</span>`)
+  }
+
+
+
+  
  
 })
 
