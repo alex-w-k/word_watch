7fc320ff96f732b70e26c044319381000ab1b3f2
Alex Koch <alex549us3@gmail.com>
2017-09-05 10:40:50 -0600

    first story done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ const $ = require('jquery')
 const url = require('./config').url
 
 document.addEventListener("DOMContentLoaded", () => {
+
+  $.getJSON(`${url}top_word`, function(data) {
+    let word = Object.keys(data.word)[0]
+    let number = Object.values(data.word)[0]
+    $('h3').replaceWith(`<h3>Top word from Word Watch API: ${word} (${number})</h3>`)
+  })
  
 })
 
